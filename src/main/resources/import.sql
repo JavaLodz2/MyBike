e088a128a4d2ff2090c74dcf1f2603f83e846932
Kamil Golis <kamil.golis@outlook.com>
2017-03-29 20:41:36 +0200

    Import.sql added for filling DB with default values

---
+++ src/main/resources/import.sql
@@ -0,0 +1,17 @@
+INSERT INTO public.stations (station_id, street) VALUES (1, 'Piotrkowska 1');
+INSERT INTO public.stations (station_id, street) VALUES (2, 'Politechniki 10');
+INSERT INTO public.stations (station_id, street) VALUES (3, 'Zielona 22');
+INSERT INTO public.stations (station_id, street) VALUES (4, 'Zgierska 50');
+INSERT INTO public.stations (station_id, street) VALUES (5, 'Nawrot 10');
+
+INSERT INTO public.bikes (bike_id, user_id) VALUES (1, null);
+INSERT INTO public.bikes (bike_id, user_id) VALUES (2, null);
+INSERT INTO public.bikes (bike_id, user_id) VALUES (3, null);
+INSERT INTO public.bikes (bike_id, user_id) VALUES (4, null);
+INSERT INTO public.bikes (bike_id, user_id) VALUES (5, null);
+
+INSERT INTO public.users (user_id, contact_number, first_name, last_name, login) VALUES (1, '600700800', 'Jan', 'Kowalski', 'Kowal');
+INSERT INTO public.users (user_id, contact_number, first_name, last_name, login) VALUES (2, '500100200', 'Marian', 'Nowak', 'Nowak01');
+INSERT INTO public.users (user_id, contact_number, first_name, last_name, login) VALUES (3, '700100300', 'Maria', 'Marchewka', 'Marchewa1');
+INSERT INTO public.users (user_id, contact_number, first_name, last_name, login) VALUES (4, '650300400', 'Patrycja', 'Ziemniak', 'PZiem');
+INSERT INTO public.users (user_id, contact_number, first_name, last_name, login) VALUES (5, '770320200', 'Przemek', 'Przygoda', 'PPAdv');

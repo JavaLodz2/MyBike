3ee5126102545a186e0b4e765c9dca33804df181
Kamil Golis <kamil.golis@outlook.com>
2017-03-30 19:03:03 +0200

    Repository User had Bike reference - now user entity do not have bike information - removed Bike from repository

--- src/main/java/pl/sda/dao/UserRepository.java
+++ src/main/java/pl/sda/dao/UserRepository.java
@@ -1,7 +1,14 @@
 package pl.sda.dao;
 
-/**
- * Created by trot on 30.03.17.
- */
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pl.sda.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    User findByLogin(String login);
+
+    User findByUserId(Integer userId);
+
 }
